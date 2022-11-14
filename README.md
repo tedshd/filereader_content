e39a531ad0764a88f17193d1f98d062572864c1e
Ted Hsu <ted.hsu@highlight.mobi>
2022-11-14 11:41:51 +0800

    [dev] Add files.

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # filereader_content
-Use FileReader API get upload file content
+
+Use FileReader API get upload file content.
+
+It can parse content split `,` and `\n` as array in **.txt** file.
+
+`test.txt`, `test1.txt`, `test2.txt` is test file.
+
+## Usage
+
+### use async await
+
+```JavaScript
+document.querySelector('input[type=file]').addEventListener('change', async (e) => {
+  console.log(e.target)
+  let res = await initFileContentAsync(e.target)
+  console.log(res)
+})
+```
+
+### normal use
+
+```JavaScript
+document.querySelector('input[type=file]').addEventListener('change', async (e) => {
+  console.log(e.target)
+  initFileContent(e.target, (item) => {
+    console.log(item)
+  })
+})
+```
---
+++ fileContent.js
@@ -0,0 +1,92 @@
+function initFileContent(inputFileDom, callback) {
+  const [file] = inputFileDom.files
+  const reader = new FileReader()
+
+  function checkResult(res) {
+    let tmpArr = []
+    let tmpArr2 = []
+    let resultArr = []
+    if (reader.result.includes('\n')) {
+      tmpArr = reader.result.split('\n')
+    }
+    for (let i = 0; i < tmpArr.length; i++) {
+      const element = tmpArr[i]
+      if (element === '\n' ||
+        element === '') {
+        continue
+      }
+      if (element.includes(',')) {
+        tmpArr2 = element.split(',')
+        for (let k = 0; k < tmpArr2.length; k++) {
+          const element2 = tmpArr2[k];
+          if (element2 === '\n' ||
+            element2 === '') {
+            continue
+          }
+          resultArr.push(element2.trim())
+        }
+        continue
+      }
+      resultArr.push(element.trim())
+    }
+    return resultArr
+  }
+
+  if (file) {
+    reader.readAsText(file)
+  }
+  reader.addEventListener('load', () => {
+    if (callback) {
+      callback(checkResult(reader.result))
+    } else {
+      console.error('initFileContent: not set callback to get item')
+    }
+  }, false)
+}
+
+function initFileContentAsync(inputFileDom) {
+  const [file] = inputFileDom.files
+  const reader = new FileReader()
+
+  function checkResult(res) {
+    let tmpArr = []
+    let tmpArr2 = []
+    let resultArr = []
+    if (reader.result.includes('\n')) {
+      tmpArr = reader.result.split('\n')
+    }
+    for (let i = 0; i < tmpArr.length; i++) {
+      const element = tmpArr[i]
+      if (element === '\n' ||
+        element === '') {
+        continue
+      }
+      if (element.includes(',')) {
+        tmpArr2 = element.split(',')
+        for (let k = 0; k < tmpArr2.length; k++) {
+          const element2 = tmpArr2[k];
+          if (element2 === '\n' ||
+            element2 === '') {
+            continue
+          }
+          resultArr.push(element2.trim())
+        }
+        continue
+      }
+      resultArr.push(element.trim())
+    }
+    return resultArr
+  }
+
+  if (file) {
+    reader.readAsText(file)
+  }
+
+  return new Promise(resolve => {
+    reader.addEventListener('load', () => {
+      console.log(reader.result)
+      resolve(checkResult(reader.result))
+    }, false)
+  })
+
+}
